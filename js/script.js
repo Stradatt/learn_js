a829b007ad729e8dc3fd515457103f6f0321cf3a
Misha <example@gmail.com>
2022-09-05 11:45:18 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,56 @@
+console.log(2 + 2 * 2 !== `8`);
+
+
+
+const isChecked = false,
+      isClose = false;
+
+console.log(isChecked || !isClose)
+/* Интерпритацияпше
+const category = 'toys';
+console.log(`https://someurl.com/${category}/5`);
+
+const user = 'Ivan';
+
+alert(`Привет, ${user}`);
+
+*/
+
+/* prompt, alert, confirm
+const answers = [];
+answers[0] = prompt('Как ваше имя?', '');
+answers[1] = prompt('Ваш пол?', '');
+answers[2] = prompt('Сколько вам лет?', '');
+
+document.write(answers);*/
+
+
+/*const answer = prompt("Вам есть 18?", "");
+console.log(+answer+5);*/
+/*const result = confirm('Are you here?');
+console.log(result);*/
+
+
+/*  объекты и массивы
+let obj = {
+    user: ['Anne', 'Fox', 'Six12'],
+    sort: 31,
+    color: '#666',
+    death: true,
+};
+let fake = 'fake';
+
+obj [fake] = 228;
+
+console.log(obj);
+
+*/
+
+    // console.log(obj.name);console
+
+// console.log(obj['name']);
+
+/*
+let arr = ['plum.png', 'orange.jpg', 'apple.png', {}, []];
+console.log(arr[1]);
+*/
